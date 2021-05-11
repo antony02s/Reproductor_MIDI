ae781237c5072dc8b465b1242d4d7b0724466cb4
Antony Sanchez <javi123482@gmail.com>
2021-05-11 14:32:50 -0500

    Actualización letra y funcion para lista de letras de canciones

--- programa.java
+++ programa.java
@@ -9,33 +9,29 @@ import java.util.Scanner;
 public class programa{
     public static void nombre_cancion(String ruta){
         //De aquí se se saca la lista de las canciones que se debe mostrar al usuario
-        File file=new File(ruta);
-        try{
-            String strIP;
-            Scanner sc=new Scanner(file);
-            int num_lines=0;
-            int j=1;
-            while(sc.hasNext()){
-                String line=sc.nextLine();
+        int j=1;
+        int num_lines=0;
+        String file=ruta;
+        try(BufferedReader br=new BufferedReader(new FileReader(file))){
+            String line;
+            while((line=br.readLine())!=null){
                 num_lines++;
-                String[] str=line.split(";");
-                strIP=Arrays.toString(str);
+                String [] str=line.split(";")
                 if(num_lines==1 || num_lines==53 || num_lines==138 || num_lines==168 ||
-                 num_lines==235 || num_lines==300 || num_lines==370 || num_lines==404 
-                || num_lines==452 || num_lines==554 || num_lines==643 || num_lines==690 ||
-                 num_lines==745 || num_lines==796 || num_lines==860 || num_lines==919 ){
-                    System.out.println("Canción "+j+" "+":");
-                    System.out.println("Nombre canción: "+str[0]);
-                    System.out.println("Autor: "+str[1]);
-                    System.out.println("Album: "+str[2]);
-                    System.out.println("Año: "+str[3]);
-                    System.out.println("Enlace canción: "+str[4]);
-                    System.out.println();
-                    j++;
-                }
+                    num_lines==235 || num_lines==300 || num_lines==370 || num_lines==404 
+                    || num_lines==452 || num_lines==554 || num_lines==643 || num_lines==690 ||
+                    num_lines==745 || num_lines==796 || num_lines==860 || num_lines==919 || num_lines==944
+                    || num_lines==1010){
+                        System.out.println("Canción "+j+" "+":");
+                        System.out.println("Nombre canción: "+str[0]);
+                        System.out.println("Autor: "+str[1]);
+                        System.out.println("Album: "+str[2]);
+                        System.out.println("Año: "+str[3]);
+                        System.out.println("Enlace canción: "+str[4]);
+                        System.out.println();
+                        j++;
+                    }
             }
-        }catch(FileNotFoundException e){
-            e.printStackTrace();
         }
     }
     public static void interfaz(){

42018891be73edbd0520608fc4f7412f57f79bec
Antony Sanchez <javi123482@gmail.com>
2021-05-10 23:20:20 -0500

    Actualización de interacción del usuario con el programa

--- programa.java
+++ programa.java
@@ -55,38 +55,153 @@ public class programa{
 		System.out.println("|                    5. Salir                                                   |");
         System.out.println("");
     }
+    public static void letra_repro_cancion(int indice_cancion){
+        File file=new File("C:\\letras_proyecto.csv");
+        try{
+            String strIP;
+            Scanner sc=new Scanner(file);
+            int num_lines=0;
+            //int j=1;
+            while(sc.hasNext()){
+                String line=sc.nextLine();
+                num_lines++;
+                if(indice_cancion==1){
+                    if(num_lines>=2 && num_lines<=51){
+                        String[] str=line.split(";");
+                        strIP=Arrays.toString(str);
+                        strIP=strIP.replace(",", "");
+                        System.out.println(strIP);
+                    }
+                }else if(indice_cancion==2){
+                    if(num_lines>=54 && num_lines<=136){
+                        String[] str=line.split(";");
+                        strIP=Arrays.toString(str);
+                        strIP=strIP.replace(",", "");
+                        System.out.println(strIP);
+                    }
+                }else if(indice_cancion==3){
+                    if(num_lines>=139 && num_lines<=166){
+                        String[] str=line.split(";");
+                        strIP=Arrays.toString(str);
+                        strIP=strIP.replace(",", "");
+                        System.out.println(strIP);
+                    }
+                }else if(indice_cancion==4){
+                    if(num_lines>=169 && num_lines<=233){
+                        String[] str=line.split(";");
+                        strIP=Arrays.toString(str);
+                        strIP=strIP.replace(",", "");
+                        System.out.println(strIP);
+                    }
+                }else if(indice_cancion==5){
+                    if(num_lines>=236 && num_lines<=298){
+                        String[] str=line.split(";");
+                        strIP=Arrays.toString(str);
+                        strIP=strIP.replace(",", "");
+                        System.out.println(strIP);
+                    }
+                }else if(indice_cancion==6){
+                    if(num_lines>=301 && num_lines<=368){
+                        String[] str=line.split(";");
+                        strIP=Arrays.toString(str);
+                        strIP=strIP.replace(",", "");
+                        System.out.println(strIP);
+                    }
+                }else if(indice_cancion==7){
+                    if(num_lines>=371 && num_lines<=402){
+                        String[] str=line.split(";");
+                        strIP=Arrays.toString(str);
+                        strIP=strIP.replace(",", "");
+                        System.out.println(strIP);
+                    }
+                }else if(indice_cancion==8){
+                    if(num_lines>=405 && num_lines<=450){
+                        String[] str=line.split(";");
+                        strIP=Arrays.toString(str);
+                        strIP=strIP.replace(",", "");
+                        System.out.println(strIP);
+                    }
+                }else if(indice_cancion==9){
+                    if(num_lines>=453 && num_lines<=552){
+                        String[] str=line.split(";");
+                        strIP=Arrays.toString(str);
+                        strIP=strIP.replace(",", "");
+                        System.out.println(strIP);
+                    }
+                }else if(indice_cancion==10){
+                    if(num_lines>=555 && num_lines<=641){
+                        String[] str=line.split(";");
+                        strIP=Arrays.toString(str);
+                        strIP=strIP.replace(",", "");
+                        System.out.println(strIP);
+                    }
+                }else if(indice_cancion==11){
+                    if(num_lines>=644 && num_lines<=688){
+                        String[] str=line.split(";");
+                        strIP=Arrays.toString(str);
+                        strIP=strIP.replace(",", "");
+                        System.out.println(strIP);
+                    }
+                }else if(indice_cancion==12){
+                    if(num_lines>=691 && num_lines<=743){
+                        String[] str=line.split(";");
+                        strIP=Arrays.toString(str);
+                        strIP=strIP.replace(",", "");
+                        System.out.println(strIP);
+                    }
+                }else if(indice_cancion==13){
+                    if(num_lines>=746 && num_lines<=794){
+                        String[] str=line.split(";");
+                        strIP=Arrays.toString(str);
+                        strIP=strIP.replace(",", "");
+                        System.out.println(strIP);
+                    }
+                }else if(indice_cancion==14){
+                    if(num_lines>=797 && num_lines<=858){
+                        String[] str=line.split(";");
+                        strIP=Arrays.toString(str);
+                        strIP=strIP.replace(",", "");
+                        System.out.println(strIP);
+                    }
+                }
+            }
+                
+        }catch(FileNotFoundException e){
+            e.printStackTrace();
+        }
+    }
     public static void main(String[] args) {
+        //se despliega la interfaz para el usuario, MENU DE OPCIONES
         interfaz();
+        // El usuario elige la opción que desea implementar
         Scanner scanner = new Scanner(System.in);
         System.out.println("Ingresa una de las opciones: ");
         String usuario = scanner.nextLine();
         int opcion_usuario=Integer.parseInt(usuario);
+        //El usuario elige la opción 1
         if(opcion_usuario==1){
             System.out.println();
             nombre_cancion("C:\\letras_proyecto.csv");
+
             Scanner scanner_2 = new Scanner(System.in);
             System.out.println("Ingrese el número del indice de una canción entre 1-14: ");
             String usuario_2 = scanner.nextLine();
             int opcion_usuario_2=Integer.parseInt(usuario_2);
-            for(int i=1;i<15;i++){
-                if(i==opcion_usuario_2){
-                    System.out.println("Buena elección");
-                    System.out.println();
-                    System.out.println("¿Te sabes la letra de la canción?");
-                    Scanner scanner_3 = new Scanner(System.in);
-                    System.out.println("Si te la sabes ingresa 1 para reproducir la canción, en caso contrario ingresa 2 para mostrar también la letra: ");
-                    String usuario_3 = scanner.nextLine();
-                    int opcion_usuario_3=Integer.parseInt(usuario_3);
-                    if(opcion_usuario_3==1){
-                        //codigo para repdorucir el audio de la canción
-                        System.out.println("funciona hasta aquí");
-                    }else{
-                        //codigo para reproducir auidio y mostrar letra
-                        System.out.println("funciona hasta aquí también");
-                    }
 
-                }
+            System.out.println();
+
+            Scanner scanner_3 = new Scanner(System.in);
+            System.out.println("Si te sabes la canción, ingresa 1 para reproducir el audio.");
+            System.out.println("En caso contrario, ingresa 2 para reproducir el audio y que se te muestre la letra.");
+            String usuario_3 = scanner_3.nextLine();
+            int opcion_usuario_3=Integer.parseInt(usuario_3);
+
+            if(opcion_usuario_3==1){
+                System.out.println("Falta el codigo para reproducir audio");
+            }else if(opcion_usuario_3==2){
+                letra_repro_cancion(opcion_usuario_2);
             }
+
         }
         
     }
